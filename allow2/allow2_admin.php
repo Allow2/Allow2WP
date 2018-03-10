6651eca7212b8d0f14d2d294b3ae9d9f691cd872
Andrew <ceo@allow2.com>
2018-03-10 17:55:34 +1000

    add header comments

--- allow2/allow2_admin.php
+++ allow2/allow2_admin.php
@@ -1,4 +1,24 @@
 <?php
+/**
+ * Plugin Name
+ *
+ * @package     Allow2
+ * @author      Allow2 Pty Ltd
+ * @copyright   2016 Allow2 Pty Ltd
+ * @license     https://www.allow2.com/developer-license/
+ *
+ * @wordpress-plugin
+ * Plugin Name: Allow2
+ * Plugin URI:  https://www.github.com/Allow2/Allow2WP
+ * Description: The Allow2 WordPress plugin is a simple plug and play drop in component that empowers your site to offer full parental freedom.
+ * Version:     1.0.0
+ * Author:      Andrew Longhorn
+ * Author URI:  https://www.allow2.com
+ * Text Domain: allow2
+ * License:     Similar to Apache 2.0
+ * License URI: https://www.allow2.com/developer-license/
+ */
+
     if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
     
 	$homeUrl = get_home_url();
